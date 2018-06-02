ae19502c32d58f13109e09822342044ed5dc1604
xiaohongrc <chenxiaohong@rcplatformhk.com>
2018-06-02 18:34:20 +0800

    Initial commit

---
+++ app/src/main/java/com/hongenit/apptem/news/NewsFragment.kt
@@ -0,0 +1,24 @@
+package com.hongenit.apptem.news
+
+import com.hongenit.apptem.R
+import com.hongenit.apptem.common.BaseFragment
+
+/**
+ * Created by Xiaohong on 2018/5/28.
+ * desc:
+ */
+class NewsFragment:BaseFragment() {
+    override fun getFragmentContentId(): Int {
+        return R.layout.fragment_news
+    }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
